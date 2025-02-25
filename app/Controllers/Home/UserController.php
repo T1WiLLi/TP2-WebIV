1d55f8397bb0cfc6bb2ea13401c32a4b0676c858
T1WiLLi <williambeaudun@gmail.com>
2025-02-24 23:03:45 -0500

    refactor(UserController): First draft of the refactored UserController using the before() lifecycle function.

--- app/Controllers/Home/UserController.php
+++ app/Controllers/Home/UserController.php
@@ -3,6 +3,7 @@
 namespace Controllers\Home;
 
 use Models\Exceptions\UserNotFound;
+use Models\Transaction\Entities\User;
 use Models\Transaction\Services\TokenService;
 use Models\Transaction\Services\TransactionService;
 use Models\Transaction\Services\UserService;
@@ -19,6 +20,16 @@ class UserController extends Controller
     private TransactionService $transactionService;
     private TokenService $tokenService;
 
+    /**
+     * @var User | null
+     */
+    private ?User $currentUser = null;
+
+    /**
+     * @var string | null
+     */
+    private ?string $currentToken = null;
+
     public function __construct()
     {
         $this->userService = new UserService();
@@ -26,124 +37,125 @@ class UserController extends Controller
         $this->tokenService = new TokenService();
     }
 
+    public function before(): ?Response
+    {
+        $token = $this->request->getArgument('token');
+        if ($token !== null) {
+            $this->currentToken = $token;
+            // Validate token; if invalid, abort
+            if (!$this->tokenService->validateToken($token)) {
+                return $this->abortForbidden("Le token est invalide.");
+            }
+
+            // Retrive user from the token
+            $userID = $this->tokenService->getUserIDFromToken($token);
+            $this->currentUser = $this->userService->getProfile($userID);
+            if (!$this->currentUser) {
+                return $this->abortNotFound("L'utilisateur n'a pas été trouvé.");
+            }
+        }
+
+        return null;
+    }
+
     #[Get("/profile/{token}")]
     public function getProfile(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) {
-            return $this->abortForbidden("Le token est invalide");
-        }
-
-        $profile = $this->userService->getProfile($this->tokenService->getUserIDFromToken($token));
-        $token = $this->tokenService->refresh($token, $profile->id);
+        $refreshedToken = $this->tokenService->refresh($this->currentToken, $this->currentUser->id);
         return $this->json([
-            "token" => $token->value,
-            "username" => $profile->username,
-            "firstname" => $profile->firstname,
-            "lastname" => $profile->lastname,
-            "email" => $profile->email,
-            "balance" => $profile->balance,
-            "type" => $profile->type,
+            "token"     => $refreshedToken->value,
+            "username"  => $this->currentUser->username,
+            "firstname" => $this->currentUser->firstname,
+            "lastname"  => $this->currentUser->lastname,
+            "email"     => $this->currentUser->email,
+            "balance"   => $this->currentUser->balance,
+            "type"      => $this->currentUser->type,
         ], 200);
     }
 
     #[Put("/profile/{token}")]
     public function updateProfile(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) {
-            return $this->abortForbidden("Le token est invalide");
-        }
-
-        $user = $this->userService->getProfile($this->tokenService->getUserIDFromToken($token));
         $data = $this->request->getBody()->getParameters();
 
         $this->userService->updateProfile(
-            $user->id,
-            $data['firstname'] ?? $user->firstname,
-            $data['lastname'] ?? $user->lastname,
-            $data['email'] ?? $user->email,
-            $data['username'] ?? $user->username
+            $this->currentUser->id,
+            $data['firstname'] ?? $this->currentUser->firstname,
+            $data['lastname'] ?? $this->currentUser->lastname,
+            $data['email'] ?? $this->currentUser->email,
+            $data['username'] ?? $this->currentUser->username
         );
 
+        $refreshedToken = $this->tokenService->refresh($this->currentToken, $this->currentUser->id);
         return $this->json([
-            "token" => $this->tokenService->refresh($token, $user->id)->value,
-            "message" => "Le profil a été mis à jour avec succès"
+            "token"   => $refreshedToken->value,
+            "message" => "Le profil a été mis à jour avec succès."
         ], 200);
     }
+
 
     #[Put("/profile/{token}/password")]
     public function changePassword(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) {
-            return $this->abortForbidden("Le token est invalide");
-        }
-
         $data = $this->request->getBody()->getParameters();
-        $user = $this->userService->getProfile($this->tokenService->getUserIDFromToken($token));
 
         try {
             $this->userService->changePassword(
-                $user->id,
+                $this->currentUser->id,
                 $data['old_password'] ?? '',
                 $data['new_password'] ?? ''
             );
         } catch (UserNotFound $e) {
-            return $this->abortNotFound("L'utilisateur n'a pas été trouvé");
+            return $this->abortNotFound("L'utilisateur n'a pas été trouvé.");
         } catch (RuntimeException $e) {
             return $this->abortBadRequest($e->getMessage());
         }
 
+        $refreshedToken = $this->tokenService->refresh($this->currentToken, $this->currentUser->id);
         return $this->json([
-            "token" => $this->tokenService->refresh($token, $user->id),
+            "token"   => $refreshedToken->value,
             "message" => "Mot de passe modifié avec succès."
-        ]);
+        ], 200);
     }
 
     #[Post("/profile/{token}/credits")]
     public function addCredits(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) { // Add as lifecycle for everywhere request
-            return $this->abortForbidden("Le token est invalide");
-        }
-
         $data = $this->request->getBody()->getParameters();
-        $user = $this->userService->getProfile($this->tokenService->getUserIDFromToken($token));
 
         try {
             $this->userService->addCredits(
-                $user->id,
+                $this->currentUser->id,
                 (float)($data['credit'] ?? 0)
             );
         } catch (UserNotFound $e) {
-            return $this->abortNotFound("L'utilisateur n'a pas été trouvé");
+            return $this->abortNotFound("L'utilisateur n'a pas été trouvé.");
         } catch (RuntimeException $e) {
             return $this->abortBadRequest($e->getMessage());
         }
+
+        $refreshedToken = $this->tokenService->refresh($this->currentToken, $this->currentUser->id);
+        return $this->json([
+            "token"   => $refreshedToken->value,
+            "message" => "Crédits ajoutés avec succès."
+        ], 200);
     }
 
     #[Get("/profile/{token}/transactions")]
     public function getTransactionHistory(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) { // Add as lifecycle for everywher
-            return $this->abortForbidden("Le token est invalide");
-        }
-
-        $history = $this->transactionService->getTransactions($this->tokenService->getUserIDFromToken($token));
+        $history = $this->transactionService->getTransactions($this->currentUser->id);
         return $this->json($history, 200);
     }
 
     #[Post("/profile/{token}/transactions")]
     public function performTransaction(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) {
-            return $this->abortForbidden("Le token est invalide");
-        }
-
         $data = $this->request->getBody()->getParameters();
-        $user = $this->userService->getProfile($this->tokenService->getUserIDFromToken($token));
 
         try {
             $this->transactionService->createTransaction(
-                $user->id,
+                $this->currentUser->id,
                 $data['name'] ?? '',
                 (float)($data['price'] ?? 0),
                 (int)($data['quantity'] ?? 0)
@@ -152,28 +164,26 @@ class UserController extends Controller
             return $this->abortBadRequest($e->getMessage());
         }
 
+        $refreshedToken = $this->tokenService->refresh($this->currentToken, $this->currentUser->id);
         return $this->json([
-            "token" => $this->tokenService->refresh($token, $user->id),
-            "message" => "Transaction crée avec succès",
+            "token"   => $refreshedToken->value,
+            "message" => "Transaction créée avec succès."
         ], 201);
     }
 
     #[Post("/profile/{token}/elevate")]
     public function elevateAccount(string $token): Response
     {
-        if (!$this->tokenService->validateToken($token)) {
-            return $this->abortForbidden("Le token est invalide");
-        }
-
         try {
-            $this->userService->elevateAccount($this->tokenService->getUserIDFromToken($token));
+            $this->userService->elevateAccount($this->currentUser->id);
         } catch (UserNotFound $e) {
-            return $this->abortNotFound("L'utilisateur n'a pas été trouvé");
+            return $this->abortNotFound("L'utilisateur n'a pas été trouvé.");
         }
 
+        $refreshedToken = $this->tokenService->refresh($this->currentToken, $this->currentUser->id);
         return $this->json([
-            "token" => $this->tokenService->refresh($token, $this->tokenService->getUserIDFromToken($token)),
+            "token"   => $refreshedToken->value,
             "message" => "Le compte est maintenant PREMIUM."
-        ]);
+        ], 200);
     }
 }
